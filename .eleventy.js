1fcd1001b4b4a2bb476fe32a8a0b982d83b4b75f
CheemsAndFriends <granchinvictor@gmail.com>
2025-03-24 04:39:52 +0100

    11ty Stuff

---
+++ .eleventy.js
@@ -0,0 +1,97 @@
+const date = require("date-and-time");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
+const urlSite = "http://localhost:8080/";
+
+const getAsset = (asset) => {
+      
+  let file = asset.split(".");
+  let extension = file[file.length - 1];
+  
+  let urlDirecter = "";
+
+  switch (extension.trim())
+  {
+    case "css": // styling shit
+      urlDirecter = "css/";
+      break;
+
+    case "png": // Images
+    case "svg":
+    case "jpg":
+    case "jpeg":
+    case "bmp":
+    case "gif":
+      urlDirecter = "images/";
+      break;
+    
+    case "js":
+    case "txt":
+      urlDirecter = "data/";
+      break;
+    case "mp3":
+      urlDirecter = "music/";
+      break;
+  }
+  return urlSite + "assets/" + urlDirecter + asset.split(" ").join("%20");
+}
+const captionImage = (vars) => {
+
+  let img = vars.split(", ");
+
+  let str = "";
+
+  let id = (img.length > 2) ? img[2] : undefined;
+  let ident = "";
+  
+  if (id != undefined)
+    ident = "id=\"" + id + "\"";
+  
+  console.trace(img);
+  // str = "<div class='captionImg' " + ident + ">";
+
+
+  str += "\n<img " + ident + " src='" + getAsset(img[0]) + "' alt=\"" + img[1] + "\" />";
+  str += "\n<span>" + img[1] + "</span>";
+
+  // str += "\n</div>";
+  return str;
+}
+
+const iconify = (dateObj) => {
+  let str = "";
+
+  for (let i = 0; i < dateObj.length; i++)
+  {
+    let url = new URL(dateObj[i]);
+    
+    let urlArr = url.hostname.split(".");
+    
+    str += '<a href="'+url.toString()+'"><img src="'+getAsset("icons/" + urlArr[urlArr.length - 2] + ".svg")+'"/></a>\n\t\t';
+  }
+
+  
+  return str;
+}
+
+const titleFilter = (title) => {
+
+  let tit = "CheemsAndFriends";
+  
+  if (title != undefined)
+    tit += ": " + title;
+
+  return tit;
+}
+
+module.exports = function(eleventyConfig) {
+  
+    eleventyConfig.addPassthroughCopy("./assets/");
+    eleventyConfig.addLayoutAlias("default", "mainLayout.html");
+
+    eleventyConfig.addFilter("iconify", iconify);
+    eleventyConfig.addFilter("getAsset", getAsset);
+    eleventyConfig.addFilter("captionImage", captionImage);
+    eleventyConfig.addFilter("titleFilter", titleFilter);
+    
+  };
---
+++ about.md
@@ -0,0 +1,35 @@
+---
+layout: postLayout.html
+title: About me
+---
+
+Im CheemsAndFriends!
+
+{{ "salutation.svg, Cheems Saluting, CheemSalute" | captionImage }}
+
+<h1 align="center">Good morning!</h1>
+
+<p align="justify">
+      I'm a <b>software developer</b>, trying to improve myself sometimes and most of times obsessing myself into a project. Whether you consider this bad or not is up to you lmao.
+  <br><br>I like playing videogames, coding and pretty much it. 
+  <br>I'm a simple guy who enjoys simple things, laughs at random memes and tries to be friendly with people, altho I'm not very good with socialising, I try my best to at least.
+  have some normal convo and make you laugh from time to time.
+<br><br>I program almost 100% in <b>Haxe</b>, an <b>open-source programming language</b>, aiming to be a <b>successor of ActionScript</b>. <b>High-level and crossplatform</b>, which is very useful if you have one piece of code in haxe and you wanna port to <b>JS, C++, Java, Python</b>, etc. Not to mention, you can use <b>"libraries"</b> which consists of <b>code that you can inject</b> on a <b>Haxe project</b> for several reasons: Maybe you need some util functions that can ease your workflow, or maybe it's an entire game engine which then you can use to create your own games!
+I personally use <b>HaxeFlixel</b>, a game engine that has been built on top of <b>OpenFL</b>: a framework that replicates all the API of <b>Flash</b> to make games!
+</p>
+
+
+<h1 align="center"> Stats </h1>
+
+
+<img style="max-width='500%';" src="https://github-readme-stats.vercel.app/api?username=CheemsAndFriends&theme=dark"></img>
+<img style="width='100%';" src="https://github-readme-stats.vercel.app/api/top-langs/?username=CheemsAndFriends&theme=dark"><img>
+<h1 align="center">Music</h1>
+
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/0CsVbYMWROqesyatfNjIqx?utm_source=generator&theme=0" max-width="max-content" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+<style>
+  #CheemSalute {
+    width: 500px;
+  }
+</style>
